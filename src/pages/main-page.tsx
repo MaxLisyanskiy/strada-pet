79a4ef0c6157855e7b72788653ae59dc28f1083b
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-12-18 22:20:06 +0300

    feat: add articles rtk-query

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,4 +1,13 @@
+import { articlesAPI } from '../services/articles-api';
+
 const MainPage = () => {
+  const { data, isLoading, isSuccess } = articlesAPI.useGetAllArticlesQuery({
+    limit: 10,
+    offset: 0,
+  });
+
+  console.log(data, isLoading, isSuccess);
+
   return <h1>Main Page</h1>;
 };
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,24 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { articlesAPI } from '../services/articles-api';
 import themeReducer from './reducers/theme-slice';
 
 const rootReducer = combineReducers({
   theme: themeReducer,
+  [articlesAPI.reducerPath]: articlesAPI.reducer,
 });
 
-const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-  },
-});
+export const setupStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(articlesAPI.middleware),
+  });
+};
 
-export default store;
+export const store = setupStore();
 
 export type RootState = ReturnType<typeof rootReducer>;
-export type AppStore = ReturnType<typeof store>;
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore['dispatch'];
+
+export default store;
