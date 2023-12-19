80cc171337b02d832d0c54ae8ca5dd933570ada9
Your Name <you@example.com>
2023-12-19 17:53:52 +0300

    fix: add / to enum path

--- src/router/types.ts
+++ src/router/types.ts
@@ -2,7 +2,7 @@ export enum AppRoutesPath {
   MAIN = '/',
   PARAGRAPH_DETAILS = '/paragraph-details',
   PROFILE = '/profile',
-  SIGN_IN = 'sign-in',
-  SIGN_UP = 'sign-up',
+  SIGN_IN = '/sign-in',
+  SIGN_UP = '/sign-up',
   NOT_FOUND_PAGE = '*',
 }
