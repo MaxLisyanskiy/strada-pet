85bd3fb41ff4759a3bceaa895f1fa3aae7ad60aa
freddypopa <gamersuper801@gmail.com>
2023-12-22 21:48:19 +0300

    feat: add sign in logic

--- src/pages/not-found-page.tsx
+++ src/pages/not-found-page.tsx
@@ -2,7 +2,7 @@ import updateMetaData from '../utils/create-meta';
 
 const NotFoundPage = () => {
   updateMetaData({ title: 'ERROR | News App', description: 'Error page' });
-  return <h1>Not Found Page</h1>;
+  return <h1>Not found page</h1>;
 };
 
 export default NotFoundPage;
